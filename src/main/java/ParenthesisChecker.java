a8d20f32811908c5d8a4d2441e55afb07b031372
Colin Farquhar <colin.farquhar@brightnetwork.co.uk>
2021-12-08 09:52:11 +0000

    add solution

--- src/main/java/ParenthesisChecker.java
+++ src/main/java/ParenthesisChecker.java
@@ -1,7 +1,37 @@
+import java.util.HashMap;
+import java.util.Stack;
+
 public class ParenthesisChecker {
 
-    /**
-     * Write your solution in this file
-     */
+    public static boolean check(String inputString){
+
+        Stack<Character> foundBrackets = new Stack<>();
+
+        HashMap<Character, Character> matchingBrackets = new HashMap<>();
+        matchingBrackets.put(')', '(');
+        matchingBrackets.put(']', '[');
+        matchingBrackets.put('}', '{');
+        matchingBrackets.put('>', '<');
+
+        char[] letters = inputString.toCharArray();
+
+        for (Character character : letters) {
+            if (character.equals('(') || character.equals('[') || character.equals('{') || character.equals('<')){
+                foundBrackets.push(character);
+            }
+            else if (character.equals(')') || character.equals(']') || character.equals('}') || character.equals('>')){
+                Character openingBracket = matchingBrackets.get(character);
+
+                if (foundBrackets.isEmpty() || !foundBrackets.peek().equals(openingBracket)){
+                    return false;
+                }
+                else {
+                    foundBrackets.pop();
+                }
+            }
+        }
+
+        return foundBrackets.isEmpty();
+    }
 
 }
